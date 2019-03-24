1f7c1f97f9fab18ecf8c84833931c5288f802c72
ezra <emb@catfact.net>
2019-03-24 09:23:47 +0700

    finish sines thing

--- sins.lua
+++ sins.lua
@@ -4,10 +4,7 @@
 
 engine.name = 'Zsins'
 
-local nob = dofile(_path.code .. 'zebra/lib/rnob.lua')
-
-local finc = 4
-local fincrand = 2
+nob = dofile(_path.code .. 'zebra/lib/rnob.lua')
 
 local OscState = {}
 OscState.__index = OscState
@@ -16,6 +13,7 @@ OscState.new = function()
   s.f = 220 -- ui linear freq
   s.ri = 1 -- ratio knob index
   s.a = 0 -- linear amp
+  s.pan = 0
   s.metaTable = OscState
   return s
 end
@@ -26,26 +24,37 @@ UiState.new = function()
   local s = {}
   s.fn = nil -- ui function array
   s.v = 1 -- voice select
+  s.e1 = 'amp' -- encoder 1 function
+  s.e2 = 'ratio' -- encoder 2 function	
   s.metaTable = UiState
   return s
 end
 
-local o = OscState.new()
+local o = {}
 local u = UiState.new()
 
-function init() 
-  -- nothing to do
+local numSines = 32
+
+function cleanup()
+  -- nothing to do?
 end
 
 -- call assigned sub-handler
 function enc(n,z) 
-  print("enc",n,z)
-  u.uifn.enc[n](z); 
-  redraw() 
+  -- print("enc",n,z)
+  if n>1 then 
+    u.fn.enc[n-1](z)
+  else
+    if z > 0 then u.v = u.v + 1
+    else u.v = u.v - 1 end
+    if u.v < 1 then u.v = 1 end
+    if u.v > numSines then u.v = numSines end
+  end
+  redraw()
 end
 function key(n,z) 
   print("key",n,z)
-  u.uifn.key[n](z); 
+  if n>1 then u.fn.key[n-1](z) end
   redraw() 
 end
 
@@ -59,45 +68,69 @@ local uifn = {
   },
 
   one = {
-    enc={
+    enc= {
       function(z)
-        o.f = o.f + finc + math.random(fincrand)
-        engine.hz(u.v, o.f * nob[o.ri])
+	      if u.e1 == 'freq' then
+          o[u.v].f = o[u.v].f + z
+          engine.hz(u.v, o[u.v].f * nob[o[u.v].ri][3])
+	      else
+	        o[u.v].a = o[u.v].a + z * 0.02
+	        if o[u.v].a < 0 then o[u.v].a = 0 end
+	        if o[u.v].a > 0.5 then o[u.v].a = 0.5 end
+	        engine.amp(u.v, o[u.v].a)
+	      end
       end,
       function(z)
-        o.ri = o.ri + z
-        if o.ri < 1 then o.ri = 1
-        elseif o.ri > #nob then o.ri = #nob
-        end 
-        engine.hz(u.v, o.f * nob[o.ri])
-      end,
-    },
+        if u.e2 == 'ratio' then
+          o[u.v].ri = o[u.v].ri + z
+          if o[u.v].ri < 1 then o[u.v].ri = 1
+          elseif o[u.v].ri > #nob then o[u.v].ri = #nob
+          end 
+          engine.hz(u.v, o[u.v].f * nob[o[u.v].ri][3])
+	      else
+	        o[u.v].pan = o[u.v].pan + z*0.1
+          if o[u.v].pan < -1 then o[u.v] = -1 end
+          if o[u.v].pan > 1 then o[u.v] = 1 end      
+	        engine.pan(u.v, o[u.v].pan)
+	      end
+    end
+    }, 
     key={
-      --- key 1: toggle amp on/off
+      --- key 1: select enc1 function
       function(z)
-        if z > 0 then return end
-        if o.a > 0 then o.a = 0
-        else o.a = 0.125 end
-        engine.amp(u.v, o.a)
+	      if z > 0 then u.e1 = 'freq'
+	      else u.e1 = 'amp' end
       end,
-      -- key 2: make amp lower, or maybe higher
+      -- key 2: select enc2 function
       function(z)
-        if z > 0 then return end
-        if o.a < 0.05 then o.a = 0.25
-        else o.a = o.a * 0.75 end
-        engine.amp(u.v, o.a)
+	      if z > 0 then u.e2 = 'pan'
+	      else u.e2 = 'ratio' end
       end
     },
     draw=function()
       screen.clear()
-      screen.move(64,40)
---      screen.level(hold==true and 4 or 15)
-      screen.font_face(10)
-      screen.font_size(20)
-      screen.text_center("ZZIN")
+      screen.font_face(1)
+      screen.font_size(8)
+      screen.move(10, 20)
+      screen.text("A = "..o[u.v].a)
+      screen.move(60, 20)
+      screen.text("R = "..nob[o[u.v].ri][1])
+      screen.move(100, 20)
+      screen.text("/ "..nob[o[u.v].ri][2])
+      screen.move(20, 60)
+      screen.text("V = "..u.v)
+      screen.move(20, 40)
+      screen.text("F = "..o[u.v].f)
+      screen.move(60, 40)
+      screen.text("P = "..o[u.v].pan)
       screen.update()
     end
   }
 }
 
-u.fn = uifn.one
+function init() 
+  u.fn = uifn.one 
+  for i=1,numSines do
+    o[i] = OscState.new()
+  end
+end
